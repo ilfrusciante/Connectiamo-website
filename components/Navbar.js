3557f7b9b1530c8ada4454a663d1af83f1219db8
ilfrusciante <rob72rom@gmail.com>
2025-04-17 21:34:46 +0200

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,1 +1,7 @@
-// Dynamic navbar with login/logout/profile
+export default function Navbar() {
+  return (
+    <nav style={{ padding: '1rem', borderBottom: '1px solid #ccc' }}>
+      <span>Connectiamo</span>
+    </nav>
+  );
+}
