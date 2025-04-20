aa482a2ec27c8d7fffd40e8760bcebaecf8b3b65
ilfrusciante <rob72rom@gmail.com>
2025-04-20 04:57:26 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: false,
- reactStrictMode: true,
+  swcMinify: false, //
+  Disattiva SWC
   }
 
 module.exports = nextConfig
