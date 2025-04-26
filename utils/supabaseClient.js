40471e14eb998610f64f6a37e65b2205541bdfd1
ilfrusciante <rob72rom@gmail.com>
2025-04-26 18:19:06 +0200

    Update supabaseClient.js

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://vjbffluxierkgtxmoxvax.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqYmZsdXhpZXJrZ3R4bW94dmF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQwNjk0NzUsImV4cCI6MjAyOTY0NTQ3NX0.1eHZ06I29YDJ6HhsAVc4Gh_4RrUwJ0B2xapni23iibM'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
