fb85adab2804daf634a6ea1aa6f9ccd889490afe
ilfrusciante <rob72rom@gmail.com>
2025-04-26 17:55:51 +0200

    Update supabaseClient.js

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://vjbffluxierkgtxmoxvax.supabase.co'; // la tua URL di Supabase
-const supabaseKey = 'process.env.NEXT_PUBLIC_SUPABASE_KEY'; // la tua key che metteremo in .env.local
+const supabaseUrl = 'https://vjbfluxiergktxmoxvax.supabase.co'; // <-- il tuo URL (già corretto)
+const supabaseKey = 'process.env.NEXT_PUBLIC_SUPABASE_KEY'; // <-- poi vediamo come impostarlo
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase;
