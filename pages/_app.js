2bf48895bdf5967c1300ca6ee00498b19ce8c806
ilfrusciante <rob72rom@gmail.com>
2025-04-30 19:10:26 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,28 @@
-import '../styles/globals.css';
-import Navbar from '../components/Navbar';
+import '../styles/globals.css'
+import Script from 'next/script'
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Navbar />
       <Component {...pageProps} />
-    </>
-  );
-}
 
-export default MyApp;
+      {/* Tawk.to Live Chat */}
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/681258ca6407aa1903369b88/1iq3qlmio';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `
+        }}
+      />
+    </>
+  )
+}
