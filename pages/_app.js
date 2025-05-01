d1255a447cb0b18344a406d16f86f477858fb628
ilfrusciante <rob72rom@gmail.com>
2025-05-01 07:28:39 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,5 @@
 import '../styles/globals.css'
-import Script from 'next/script'
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-
-      {/* Tawk.to Live Chat */}
-      <Script
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-              s1.async=true;
-              s1.src='https://embed.tawk.to/681258ca6407aa1903369b88/1iq3qlmio';
-              s1.charset='UTF-8';
-              s1.setAttribute('crossorigin','*');
-              s0.parentNode.insertBefore(s1,s0);
-            })();
-          `
-        }}
-      />
-    </>
-  )
+  return <Component {...pageProps} />
 }
