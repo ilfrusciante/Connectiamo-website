2a561344a569534fdbc0c2fc0ac71a694f4ac10f
ilfrusciante <rob72rom@gmail.com>
2025-05-03 19:47:45 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,29 @@
-import '../styles/globals.css'
+// pages/_app.js
+import { useEffect } from 'react';
+import { supabase } from '../utils/supabaseClient';
+import '../styles/globals.css';
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const updateLastSeen = async () => {
+      const { data: { user } } = await supabase.auth.getUser();
+      if (user) {
+        await supabase
+          .from('profiles')
+          .update({ last_seen: new Date().toISOString() })
+          .eq('id', user.id);
+      }
+    };
+
+    // aggiorna subito
+    updateLastSeen();
+
+    // aggiorna ogni 30 secondi
+    const interval = setInterval(updateLastSeen, 30000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
