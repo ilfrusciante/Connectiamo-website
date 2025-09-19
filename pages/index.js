ee02a973dff1fe406fd9cafdd1f70c9f93091d92
ilfrusciante <rob72rom@gmail.com>
2025-09-19 07:32:34 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -131,7 +131,7 @@ export default function Home() {
                   <h3 className="text-xl font-semibold mb-4 text-gray-800">3 Avvia la collaborazione</h3>
                 </div>
                 <p className="text-gray-600 text-base pb-2 min-h-[120px]">
-                  Trova il contatto giusto e gestisci tutto in modo semplice e riservato direttamente dalla piattaforma.
+                  Trova il contatto giusto e gestisci tutto in modo semplice e riservato dalla piattaforma.
                 </p>
               </div>
             </div>
