b89622d473819dc87b1b40903a736c88350d7428
ilfrusciante <rob72rom@gmail.com>
2025-09-27 08:52:45 +0200

    Update signup.js

--- pages/signup.js
+++ pages/signup.js
@@ -232,7 +232,6 @@ export default function Signup() {
             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required placeholder="Password" className="w-full px-3 py-2 rounded bg-gray-700 text-white" />
 
             <div>
-              <label className="block mb-1 text-sm">Ruolo</label>
               <input
                 type="text"
                 value="Collaborazione"
